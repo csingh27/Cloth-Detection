0b38674c96a6dfc52c57b349f566cdfa97642472
csingh27 <singhchandandeep34@gmail.com>
2020-11-19 22:29:28 +0100

    Changed name train.py to dataset_generation.py and modified it (dataset_display use^Cfor df_train and df_val), updated readme (sequence of events and model training process updated)

--- README.md
+++ README.md
@@ -18,18 +18,33 @@ Dataset of
 4. Convert dataset to a model compatible format✓  
 5. Train-test split✓  
 6. Data pre-processing✓  
-4. Data Augmentation  
-5. Display Augmented data  
-6. Train-test split  
-8. Train the model  
-9. Test the result  
-10. Inference  
+7. Model training✓  
+9. Inference✓  
  
-*Installation requirements :*  
+*Package Installation requirements :*  
 Torch -  
-python3 -m pip install torch==1.4.0+cu92 torchvision==0.5.0+cu92 -f https://download.pytorch.org/whl/torch_stable.html  
+pip install torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 Opencv -  
 pip3 install opencv-python  
+Yolo v5 -  
+git clone  'https://github.com/ultralytics/yolov5.git'  
+Pandas -  
+pip3 install pandas
+YAML -  
+pip3 install pyyaml
+Tensorboard -  
+pip3 install tensorboard
+Matplotlib -  
+pip3 install matplotlib
+
+*Model Training :*   
+* Train model
+python3 train.py --img 224 --batch 4 --epochs 2     --data /home/chandandeep/GitHub/Coding-Challenge-Sewts/Model/bcc.yaml --cfg /home/chandandeep/GitHub/Coding-Challenge-Sewts/Model/yolov5s.yaml --name BCCM
+* Visualize using Tensorboard
+tensorboard --logdir=runs
+
+*Model Inference :*   
+python yolov5/detect.py --source ~/Coding-Challenge-Sewts/Dataset/Dataset_yolo/images/val  --weights '~/yolov5/runs/train/BCCM18/weights/best.pt'
 
 
 *Challenge Evaluation criteria :*   
@@ -40,4 +55,7 @@ pip3 install opencv-python
 • Code reusability  
 • Testing procedures  
 • Consistent use of a version control system (i.e. git)  
+
+
+
 
--- dataset_generate.py
+++ dataset_generate.py
@@ -1,7 +1,7 @@
 # IMPORTING LIBRARIES
 
 
-# * General
+# * General libraries
 import cv2
 import os
 import glob
@@ -10,52 +10,58 @@ import numpy as np
 import shutil
 from shutil import copyfile
 
-# * ML framework
+# * ML specific libraries
 import torch
 import torchvision
 from torch.utils.data import DataLoader
-
-# * ML model
 from sklearn import preprocessing, model_selection
 
 # Function to split training and test set
 
-
-# Function to convert Dataset into YoloV5 compatible format 
 def dataset_split(df, folder, train_img_path, train_label_path):
 	filenames = []
-	
 	for name in df.file_name:
 		filenames.append(name)
+	# Directory Structure :
+	# --Dataset_yolo
+	# 	 --Images
+	# 	 	--Train
+	# 	 	--Val
+	# --Dataset_yolo
+	# 	 --Labels
+	# 	 	--Train
+	# 	 	--Val
+	# Image format .jpg, Label format .txt (Separate .txt file label for each image)
+	# Inside label.txt : x_center_norm, y_center_norm, width_norm, height_norm
 	for filename in filenames:
 		yolo_list = []
-		for _,row in df[df.file_name == filename].iterrows():
-			yolo_list.append([row.file_name, row.x_center_norm, row.width_norm, row.y_center_norm, row.height_norm])
+		for i,row in df[df.file_name == filename].iterrows():
+			yolo_list.append([0,row.x_center_norm, row.y_center_norm, row.width_norm, row.height_norm])
 		yolo_list = np.array(yolo_list)
 		print("\n",yolo_list)
 		txt_filename = os.path.join(train_label_path,str(row.file_name.split('.')[0])+".txt")
 		print("\n",txt_filename)
-		np.savetxt(txt_filename, yolo_list, fmt="%s")
+		np.savetxt(txt_filename, yolo_list, fmt=["%d","%f","%f","%f","%f"])
 		shutil.copyfile(os.path.join(folder,row.file_name), os.path.join(train_img_path,row.file_name))
-
-
 
 	return(0)
 
+# Function to convert Dataset into YoloV5 compatible format 
 
 def convert_dataset(path,table):
+	# YoloV5 compatible dataset has x_center_norm, y_center_norm, width_norm, height_norm as its columns
 	img_width = 224
 	img_height = 224
 	width=[]
 	height=[]
 	x_center=[]
 	y_center=[]
-	df = pd.DataFrame(columns=['file_name','x_center_norm','width_norm','y_center_norm','height_norm'])
-	df["file_name"]=table["Unnamed: 0"]
+	df = pd.DataFrame(columns=['file_name','x_center_norm','y_center_norm','width_norm','height_norm'])
+	df["file_name"]=table['Unnamed: 0'].astype(str)
 	df["width_norm"]=(table["2"]-table["0"])/img_width
 	df["height_norm"]=(table["3"]-table["1"])/img_height
-	df["x_center_norm"]=(table["0"]+df["width_norm"])/img_width
-	df["y_center_norm"]=(table["1"]+df["height_norm"])/img_height
+	df["x_center_norm"]=(table["0"]/img_width)+(df["width_norm"]/2)
+	df["y_center_norm"]=(table["1"]/img_height)+(df["height_norm"]/2)
 	print(df)
 	df.to_csv(os.path.join(path,'Dataset/Dataset_yolo/BB_labels_yolo.txt'))
 	return(df)
@@ -63,6 +69,7 @@ def convert_dataset(path,table):
 # Function to load dataset
 
 def display_dataset_images(folder, table):
+	print(table)
 	images=[]
 	image_path=[]
 	filename=[]
@@ -76,23 +83,36 @@ def display_dataset_images(folder, table):
 	print("Displaying dataset images ... \n")
 	for i in range(len(table.index)):
 		print("Image",i)
-		image_path.append(table.loc[i,"Unnamed: 0"])
+		image_path.append(table.iloc[i,0])
 		image_path[i]=os.path.join(folder,image_path[i])
-		x1.append(table.loc[i,"0"])
-		y1.append(table.loc[i,"1"])
-		x2.append(table.loc[i,"2"])
-		y2.append(table.loc[i,"3"])
+		print(image_path[i])
+		# Gets xmin, ymin, xmax, ymax when Table (dataset_raw) is passed as argument
+		# Gets x_center_norm, y_center_norm, width_norm, height_norm when DF (dataset_yolo) is passed as argument
+		x1.append(table.iloc[i,1])
+		y1.append(table.iloc[i,2])
+		x2.append(table.iloc[i,3])
+		y2.append(table.iloc[i,4])
+		# De-normalizes in the case when DF is passed as argument
+		if "x_center_norm" in table:
+			image_path[i]=os.path.join(folder,table.iloc[i,0])
+			print(image_path[i])
+			x1[i]=int(224*(x1[i]-x2[i]/2)) #224(x_center-width/2)
+			y1[i]=int(224*(y1[i]-y2[i]/2)) #224(x_center-height/2)
+			x2[i]=int(224*x2[i]+x1[i]-x2[i]/2) #224(width+x_center-width/2)
+			y2[i]=int(224*y2[i]+y1[i]-y2[i]/2) #224(width+x_center-height/2)
 		start_point.append((x1[i],y1[i]))
 		print("Bounding box \n(xmin,ymin)",start_point[i])
 		end_point.append((x2[i],y2[i]))
 		print("(xmax,ymax)",end_point[i])
 		img=cv2.imread(image_path[i])
 		img = cv2.rectangle(img, start_point[i],end_point[i], color=(255, 0, 0), thickness=2) 
-		cv2.waitKey(1)
+		cv2.waitKey(100)
+		# Displays images
 		cv2.imshow('image',img)
 		if img is not None:
 			images.append(img)
 		i=i+1
+	cv2.waitKey(500)
 	return(images,start_point,end_point)
 
 
@@ -112,11 +132,12 @@ def main():
     table = pd.read_csv(csv_path) 
     print(table)
     # Display dataset
+    print("Raw dataset ... \n")
     display_dataset_images(folder,table)
     # Convert dataset to Yolo Compatible
     df=convert_dataset(path,table)
-    # Training-validation split
-    df_train, df_valid = model_selection.train_test_split(df, test_size=0.1, random_state=13, shuffle=True)
+    # Train-test split
+    df_train, df_valid = model_selection.train_test_split(df, test_size=0.2, random_state=13, shuffle=True)
     train_img_path = os.path.join(path,'Dataset/Dataset_yolo/images/train')
     train_label_path = os.path.join(path,'Dataset/Dataset_yolo/labels/train')
     valid_img_path = os.path.join(path,"Dataset/Dataset_yolo/images/val")
@@ -127,12 +148,12 @@ def main():
     print("No. of Training labels", len(os.listdir(train_label_path)))
     print("No. of valid images", len(os.listdir(valid_img_path)))
     print("No. of valid labels", len(os.listdir(valid_label_path)))
+    #Display converted dataset for verification
+    print("Train dataset ... \n")
+    display_dataset_images(train_img_path,df_train)
+    print("Validation dataset ... \n")
+    display_dataset_images(valid_img_path,df_valid)
 
 if __name__ == '__main__':
     main()
-
-
-#models=torchvision.models.detectron.faster_rcnn_resnet150_fpm(pretrained=True)
-#train_set, val_set = torch.utils.data.random_split(dataset, [50000, 10000])
-
 
